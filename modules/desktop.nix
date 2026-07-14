faa16aeed97e3a3bedfc5854af890a1c2ac814bc
0xjibs <jb.stilhart@proton.me>
2026-07-14 12:33:24 +0200

    modularisation test

---
+++ modules/desktop.nix
@@ -0,0 +1,84 @@
+{self, ... }: {
+  flake.nixosModules.desktop = {pkgs, ...}: let
+    selfpkgs = self.packages."${pkgs.system}";
+  in {
+    imports = [
+      self.nixosModules.mango
+
+      self.nixosModules.audio
+      #self.nixosModules.firefox
+      #self.nixosModules.chromium
+    ];
+    
+    #inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
+    #programs.niri.enable = true;
+    #programs.niri.package = selfpkgs.niri;
+
+    # preferences.autostart = [selfpkgs.quickshellWrapped];
+    preferences.autostart = [selfpkgs.noctalia-shell];
+
+    environment.systemPackages = [
+      selfpkgs.terminal
+      pkgs.pcmanfm
+      selfpkgs.noctalia-shell
+    ];
+
+    fonts.packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
+      ubuntu-sans
+      cm_unicode
+      corefonts
+      unifont
+      noto-fonts
+		  noto-fonts-cjk-sans
+		  noto-fonts-color-emoji
+		  fira-code
+		  fira-code-symbols
+		  departure-mono
+    ];
+
+    fonts.fontconfig.defaultFonts = {
+      serif = ["Ubuntu Sans"];
+      sansSerif = ["Ubuntu Sans"];
+      monospace = ["JetBrainsMono Nerd Font"];
+    };
+
+    # Set your time zone.
+    time.timeZone = "Europe/Paris";
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = "fr_FR.UTF-8";
+
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = "fr_FR.UTF-8";
+        LC_IDENTIFICATION = "fr_FR.UTF-8";
+        LC_MEASUREMENT = "fr_FR.UTF-8";
+        LC_MONETARY = "fr_FR.UTF-8";
+        LC_NAME = "fr_FR.UTF-8";
+        LC_NUMERIC = "fr_FR.UTF-8";
+        LC_PAPER = "fr_FR.UTF-8";
+        LC_TELEPHONE = "fr_FR.UTF-8";
+        LC_TIME = "fr_FR.UTF-8";
+    };
+    
+    # Install Wayland
+    programs.xwayland.enable = true;
+    programs.firefox.enable = true;
+
+    services.upower.enable = true;
+
+    security.polkit.enable = true;
+
+    hardware = {
+      enableAllFirmware = true;
+
+      bluetooth.enable = true;
+      bluetooth.powerOnBoot = true;
+
+      opengl = {
+        enable = true;
+        driSupport32Bit = true;
+      };
+    };
+  };
+}
